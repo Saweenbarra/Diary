ff71325ebc183452ea00f8422677e0a3a6b06b4c
Saweenbarra <adamfrew1999@gmail.com>
2020-07-14 22:53:43 +0100

    Added Diary.csv and reading contents

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main () {
-    cout << "HellowWorld!" << endl;
-    cin.get();
+int main() {
+	ifstream input("../Diary.csv");
+	if (!input) cerr << "Could not open the file!" << endl;
+
+	ifstream myFile;
+	myFile.open("../Diary.csv");
+
+	while (myFile.good()) {
+		string line;
+		getline(myFile, line, ',');
+		cout << line << endl;
+	}
+	std::cin.get();
+	myFile.close();
 }
